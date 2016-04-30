199fb6f542afdc069c6c50e61162112703881d75
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2016-04-29 18:32:09 -0700

    Start cleaning up Ask Jacky View
    Started making the Ask Jacky view look better

--- DearJacky/mobile/src/main/java/com/dearjacky/RespondActivity.java
+++ DearJacky/mobile/src/main/java/com/dearjacky/RespondActivity.java
@@ -2,8 +2,11 @@ package com.dearjacky;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.alorma.timeline.TimelineView;
@@ -15,13 +18,13 @@ public class RespondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_respond);
         android.support.v7.app.ActionBar a = getSupportActionBar();
-        a.setTitle("Jacky");
+        a.setTitle("Ask Jacky");
         a.setBackgroundDrawable(new ColorDrawable( ContextCompat.getColor(this, R.color.colorPrimary)));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Typewriter jackyText = (Typewriter) findViewById(R.id.event_name);
         jackyText.setCharacterDelay(50);
         jackyText.animateText("Here are some things that have made you happy before!");
-        ListView list = (ListView) findViewById(R.id.activity_list);
 
         Typewriter jackyText2 = (Typewriter) findViewById(R.id.jacky_text2);
         jackyText2.setInitialDelay(1500);
@@ -36,8 +39,23 @@ public class RespondActivity extends AppCompatActivity {
                     TimelineView.TYPE_MIDDLE, (int)(Math.random()*4)));
         }
         items.add(new Events("Last Event", TimelineView.TYPE_END, (int)(Math.random()*4)));
-        list.setAdapter(new EventsAdapter(this, items));
 
         list2.setAdapter(new EventsAdapter(this, items));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (getParentActivityIntent() == null) {
+                    Log.i("T", "You have forgotten to specify the parentActivityName in the AndroidManifest!");
+                    onBackPressed();
+                } else {
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
