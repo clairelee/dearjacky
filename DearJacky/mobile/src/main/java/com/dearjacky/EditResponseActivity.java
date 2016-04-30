64eb986ab4c8775ea132e3b5af9668ed3b1eb542
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2016-04-30 15:09:22 -0700

    Action bar for edit/new response updated

--- DearJacky/mobile/src/main/java/com/dearjacky/EditResponseActivity.java
+++ DearJacky/mobile/src/main/java/com/dearjacky/EditResponseActivity.java
@@ -3,10 +3,15 @@ package com.dearjacky;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class EditResponseActivity extends AppCompatActivity {
 
@@ -14,9 +19,19 @@ public class EditResponseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_response);
-
+        String title;
+        // If there are extras, edit, if not new...
+        Intent intent = getIntent();
+        if (intent.hasExtra("event_id")) {
+            title = "Edit Response";
+            // Do stuff to get the event infos
+        } else {
+            title = "New Response";
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
         android.support.v7.app.ActionBar a = getSupportActionBar();
-        a.setTitle("Edit Response");
+        a.setTitle(title);
         a.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimary)));
 
 
@@ -33,5 +48,33 @@ public class EditResponseActivity extends AppCompatActivity {
 //        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#009688"));
 //        a.setBackgroundDrawable(colorDrawable);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        //return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (getParentActivityIntent() == null) {
+                    Log.i("T", "You have forgotten to specify the parentActivityName in the AndroidManifest!");
+                    onBackPressed();
+                } else {
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                return true;
+            case R.id.action_confirm:
+                // Either update the entry, or save the new entry
+                Toast.makeText(EditResponseActivity.this, "Response Saved!", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
